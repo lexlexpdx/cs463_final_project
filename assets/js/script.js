8301cebfc0f7a805685e0b9ae4baf909a0fc9f04
Lex Albrandt <lex.albrandt@gmail.com>
2026-03-12 18:16:58 -0700

    Add JS functionality for contact form 	- Added basic form validation with JavaScript 	- Checked for functionality 	- Last to-do: do any code refactoring for efficiency

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,58 @@ navbarToggle.addEventListener("click", () => {
 	navbarToggle.classList.toggle("active");
 	navbarMenu.classList.toggle("active");
 });
+
+/*
+=============================================
+Contact form actions
+=============================================
+
+Sources: 
+email validation regex: https://www.geeksforgeeks.org/javascript/javascript-program-to-validate-an-email-address/
+phone validation regex: https://www.geeksforgeeks.org/javascript/how-to-validate-phone-numbers-using-javascript/
+
+*/
+
+const contactForm = document.querySelector(".contact-container form");
+const emailInput = document.querySelector("#email");
+const submitButton = document.querySelector('button[type="submit"]');
+const nameInput = document.querySelector("#name");
+const companyInput = document.querySelector("#company");
+const phoneInput = document.querySelector("#phone");
+
+function isValidEmail(value) {
+	return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+}
+
+function isValidPhoneNumber(phone) {
+	const regex = /^\d{3}-\d{3}-\d{4}$/;
+	return regex.test(phone);
+}
+
+function validateForm() {
+	const valid =
+		isValidEmail(emailInput.value.trim()) &&
+		nameInput.value.trim() !== "" &&
+		companyInput.value.trim() !== "" &&
+		isValidPhoneNumber(phoneInput.value.trim());
+
+	return valid;
+}
+
+contactForm.addEventListener("submit", (event) => {
+	event.preventDefault();
+	validateForm();
+
+	if (submitButton.disabled) {
+		alert(
+			"Please complete all fields and ensure phone format is 555-555-5555. Try again.",
+		);
+		return;
+	}
+
+	alert("Form submitted successfully.");
+	contactForm.reset();
+	validateForm();
+});
+
+validateForm();
